f51741cee1a542eeaf3ab536dc004a139c23e194
Petran Laurentiu <56577088+petranlaurentiu@users.noreply.github.com>
2023-05-10 12:31:31 +0300

    Initialize PRIVATE_BRANCH_nextjs-preview-mode

---
+++ src/pages/[slug].page.tsx
@@ -0,0 +1,125 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
+import { useTranslation } from 'next-i18next';
+
+import { getServerSideTranslations } from './utils/get-serverside-translations';
+
+import { useBlogPostPage, useLandingPage } from '@src/_ctf-private';
+import { CtfXrayFrameDynamic } from '@src/_ctf-private/ctf-xray';
+import { ArticleContent, ArticleHero, ArticleTileGrid } from '@src/components/features/article';
+import { SeoFields } from '@src/components/features/seo';
+import { Container } from '@src/components/shared/container';
+import { client, previewClient } from '@src/lib/client';
+import { revalidateDuration } from '@src/pages/utils/constants';
+
+const Page = ({
+  blogPost: ssrBlogPost,
+  isFeatured: ssrIsFeatured,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { t } = useTranslation();
+
+  /**
+   * TODO: this is a main-private feature, and should be removed from the main branch during the split
+   */
+  const { data: blogPost } = useBlogPostPage({ slug: ssrBlogPost.slug, initialData: ssrBlogPost });
+  const { data: landingPage } = useLandingPage({
+    initialData: undefined,
+    customKey: 'landingBlogDetailPage',
+  });
+
+  const isFeatured = landingPage?.featuredBlogPost?.slug === blogPost?.slug || ssrIsFeatured;
+  const relatedPosts = blogPost?.relatedBlogPostsCollection?.items;
+
+  if (!blogPost || !relatedPosts) return null;
+
+  return (
+    <CtfXrayFrameDynamic entry={blogPost}>
+      {blogPost.seoFields && <SeoFields {...blogPost.seoFields} />}
+      <Container>
+        <ArticleHero article={blogPost} isFeatured={isFeatured} isReversedLayout={true} />
+      </Container>
+      <Container className="mt-8 max-w-4xl">
+        <ArticleContent article={blogPost} />
+      </Container>
+      {relatedPosts && (
+        <Container className="mt-8 max-w-5xl">
+          <h2 className="mb-4 md:mb-6">{t('article.relatedArticles')}</h2>
+          <ArticleTileGrid className="md:grid-cols-2" articles={relatedPosts} />
+        </Container>
+      )}
+    </CtfXrayFrameDynamic>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async ({ params, locale, preview }) => {
+  if (!params?.slug || !locale) {
+    return {
+      notFound: true,
+      revalidate: revalidateDuration,
+    };
+  }
+
+  const gqlClient = preview ? previewClient : client;
+
+  try {
+    const [blogPageData, landingPageData] = await Promise.all([
+      gqlClient.pageBlogPost({ slug: params.slug.toString(), locale, preview }),
+      gqlClient.pageLanding({ locale, preview }),
+    ]);
+
+    const blogPost = blogPageData.pageBlogPostCollection?.items[0];
+    const landingPage = landingPageData.pageLandingCollection?.items[0];
+
+    const isFeatured = landingPage?.featuredBlogPost?.slug === blogPost?.slug;
+
+    if (!blogPost) {
+      return {
+        notFound: true,
+        revalidate: revalidateDuration,
+      };
+    }
+
+    return {
+      revalidate: revalidateDuration,
+      props: {
+        ...(await getServerSideTranslations(locale)),
+        blogPost,
+        isFeatured,
+      },
+    };
+  } catch {
+    return {
+      notFound: true,
+      revalidate: revalidateDuration,
+    };
+  }
+};
+
+export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
+  const dataPerLocale = locales
+    ? await Promise.all(
+        locales.map(locale => client.pageBlogPostCollection({ locale, limit: 100 })),
+      )
+    : [];
+
+  const paths = dataPerLocale
+    .flatMap((data, index) =>
+      data.pageBlogPostCollection?.items.map(blogPost =>
+        blogPost?.slug
+          ? {
+              params: {
+                slug: blogPost.slug,
+              },
+              locale: locales?.[index],
+            }
+          : undefined,
+      ),
+    )
+    .filter(Boolean);
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
+
+export default Page;
---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,26 @@
+import { Urbanist } from '@next/font/google';
+import { appWithTranslation } from 'next-i18next';
+import './utils/globals.css';
+import type { AppProps } from 'next/app';
+
+import { CtfCustomQueryClientProvider } from '@src/_ctf-private';
+import { CtfSegmentAnalytics } from '@src/_ctf-private/ctf-analytics';
+import { Layout } from '@src/components/templates/layout';
+
+const urbanist = Urbanist({ subsets: ['latin'], variable: '--font-urbanist' });
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <CtfCustomQueryClientProvider>
+      <CtfSegmentAnalytics />
+      <main className={`${urbanist.variable} font-sans`}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </main>
+      <div id="portal" className={`${urbanist.variable} font-sans`} />
+    </CtfCustomQueryClientProvider>
+  );
+};
+
+export default appWithTranslation(App);
